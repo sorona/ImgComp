3c85271a10feaeedf152f8cbe1712ce0d8926350
sorona <sorona@t2.technion.ac.il>
2014-05-22 10:30:46 +0300

    Struct wrtie/read compleated (also stream)

--- filewrite_test.m
+++ filewrite_test.m
@@ -4,31 +4,54 @@ function filewrite_test(file_name)
     test_file_name = 'test_write';
     
     
-    Spar.counts = [1 2 21 ];
+    Spar.counts = [1;2;21 ];
     Spar.len    = 213212;
-    Spar.header.field = {'counts','len'};
-    Spar.header.len   = {length(Spar.counts),length(Spar.len)};
-    Spar.header.type  = {'uint16','uint16'};
+    Spar.lewew  = [321; 213 ; 123221];
+    Spar.header.field = {'counts','len','lewew'};
+    Spar.header.len   = {length(Spar.counts),length(Spar.len),length(Spar.lewew)};
+    Spar.header.type  = {'uint16','uint32','uint16'};
     
     fid    = fopen('test_write','w');
-    % write struct parameters 
-    field = Spar.header.field;  
-    len   = Spar.header.len;
-    type  = Spar.header.type;
-    for i=1:length(Spar.header.field)
-        fwrite(fid,len{i},'uint16');
-        fwrite(fid,eval(sprintf('Spar.%s',field{i})),type{i});
-    end
+    write_struct2file(fid,Spar);
     filesize = ftell(fid)
     fclose(fid);
     
-    Spar2.header.field = Spar2.header.field; 
-    % read struct
+    Spar2.header.field = Spar.header.field; 
+    Spar2.header.type  = Spar.header.type;
+    Spar2.header.len   = cell(1,length(Spar.header.field));
+    
     fid   = fopen('test_write','r');
-    for i=1:length(Spar.header.field)
-        Spar.headerlenfread(fid,len{i},'uint16');
-        fwrite(fid,eval(sprintf('Spar.%s',field{i})),type{i});
+    Spar2 = read_structfromfile(fid,Spar2);
+    filesize = ftell(fid)
+    fclose(fid);
+    Spar2
+    Spar
+    
+    % write struct parameters fucntion
+    function write_struct2file(fid,Spar)
+        field = Spar.header.field;  
+        len   = Spar.header.len;
+        type  = Spar.header.type;
+        for i=1:length(Spar.header.field)
+            fwrite(fid,len{i},'uint16');
+            fwrite(fid,eval(sprintf('Spar.%s',field{i})),type{i});
+        end
     end
-    pad    = fread(fid,1,'uint8');
+
+    
+    % read struct
+    function SparRe = read_structfromfile(fid,Spar)
+        field_no = length(Spar.header.field);
+        field = Spar.header.field;  
+        type  = Spar.header.type;
+        len   = cell(1,length(field));
+        for i=1:field_no
+            len{i} = fread(fid,1,'uint16');
+            eval(sprintf('SparRe.%s=fread(fid,len{%d},type{%d});',field{i},i,i)); 
+        end
+        SparRe.header.field = field;
+        SparRe.header.len   = len;
+        SparRe.header.type  = type;
+    end
     
 end

ff9818caa514c7aecc72eefaf0b1627c81070229
sorona <sorona@t2.technion.ac.il>
2014-06-07 16:46:10 +0300

    bug fix in unary coding

--- unary_coder_experiment.m
+++ unary_coder_experiment.m
@@ -1,17 +1,24 @@
-function packbits_experiment()
+function unary_coder_experiment()
 % Packedbits encoding
 load('HddSamp.mat')
+fprintf('******Unary coder experiment *****\n')
 Diff = Hdd{1}.GAMMA.Diff;
 figure;hist(Diff,604);
 % test 
 %  Diff= [2 1 6];
 %  Diff=[ 0 1 2];
+ 
+%  Diff=[ 0 1 2 5  10 2 0 1 2131 -4];
+
 % unary encoding
+ diffSign = Diff<0;
+ diffAbs = abs(Diff);
 unStream = [];
 for i=1:length(Diff)
     unStream = [unStream ones(1,Diff(i)) 0];
 end
-unStream;
+
+
 DiffRe  =[];
 ReNum=0;
 for i=1:length(unStream)
@@ -23,12 +30,14 @@ for i=1:length(unStream)
             ReNum = ReNum+1;
     end
 end
+ind    = diffSign==1;
+diffRe = diffAbs; diffRe(ind) = -(diffRe(ind));
+
 DiffRe;
-length(Diff)/length(unStream)
-
+length(Diff)/length(unStream);
+isequal(Diff,DiffRe)
 % arith encoding 
-
-
+fprintf('******Unary coder experiment done*****\n')
 
 
 end
